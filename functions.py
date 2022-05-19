ebc2ec7efbe30b3aaf4fa1b761497f28b613695b
wtrmlln <ama20012001@gmail.com>
2022-05-19 16:31:10 +0400

    first commit

---
+++ functions.py
@@ -0,0 +1,83 @@
+from tapi_yandex_metrika import YandexMetrikaLogsapi
+from data import sites_dict, sites_goals_dict, visits_columns_dict, hits_columns_dict
+from __init__ import token, date1, date2, target
+
+import datetime
+import pandas as pd
+from functools import partial
+
+def get_ym_data(site_id):
+    client = YandexMetrikaLogsapi(token=token, default_url_params={'counterId': site_id})
+    
+    visits_fields_ym = ",".join(list(visits_columns_dict.keys()))
+    hits_fields_ym = ",".join(list(hits_columns_dict.keys()))
+
+    #Создаем запрос
+    if target == 'визиты':
+        params={"date1":date1,
+                "date2":date2,
+                "fields": visits_fields_ym,
+                "source":"visits"}
+    elif target == 'просмотры':
+        params={"date1":date1,
+                "date2":date2,
+                "fields": hits_fields_ym,
+                "source":"hits"}
+    result = client.create().post(params=params)
+    request_id = result["log_request"]["request_id"]
+
+    # Если запрос подготовлен - скачать и обработать в датафрейм
+    # В ином случае - ячейка выполняется до тех пор пока не будет подготовлен запрос
+    while True:
+        info = client.info(requestId=request_id).get()
+        if info['log_request']['status'] == 'processed':
+            filename = str('YaMetrika ' + target + ' ' + sites_dict[site_id] + ' ' + datetime.datetime.now().strftime('%Y-%m-%d') + '.csv')
+            print(filename + ' подготовлен к скачиванию')
+            part = client.download(requestId=request_id, partNumber=0).get()
+            data = part.data
+            data = [x.split('\t') for x in data.split('\n')[:-1]]
+            df_ym = pd.DataFrame(data[1:], columns=data[0])
+            df_ym = df_ym.rename(columns=lambda x: x.strip())
+            
+            if target == 'visits':
+                df_ym = df_ym.rename(columns = visits_columns_dict)
+            elif target == 'hits':
+                df_ym = df_ym.rename(columns = hits_columns_dict)
+            break
+        else:
+            pass
+    #Преобразовывает числовые идентификаторы в текст и скачивает таблицу в формате xlsx    
+    
+    df_ym['Идентификаторы достигнутых целей'] = df_ym['Идентификаторы достигнутых целей'].apply(partial(get_id_names, site_id=site_id))
+    print(filename + ' успешно обработан в DataFrame')
+    return [df_ym, filename]
+
+# Переводит числовые идентификаторы (219152671) целей в текстовые (Клик по кнопке "Прямой эфир")
+def get_id_names(text, site_id):
+    new_text = ''
+    text = text.replace('[', '')
+    text = text.replace(']', '')
+    id_array = text.split(sep = ',')
+    if '' == id_array[0]:
+        return None
+    for id in id_array:
+        temptext = sites_goals_dict[site_id][id]
+        if new_text == '':
+            new_text = temptext
+        else:
+            new_text = new_text + ',' + temptext
+    return new_text
+
+def get_status_all_reports(site_id):
+    client = YandexMetrikaLogsapi(token=token, default_url_params={'counterId': site_id}) 
+    print(client.allinfo().get())
+
+# Очистить все подготовленные репорты.
+def clearout_all_reports(site_id):
+    client = YandexMetrikaLogsapi(token=token, default_url_params={'counterId': site_id})
+    result = client.allinfo().get()
+    for report in result['requests']:
+        if report['status'] == 'processed':
+            request_id = report['request_id']
+            new_result = client.clean(requestId=request_id).post()
+    print(result)
---
+++ user_input_functions.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from data import sites_dict
+
+# Получить токен с пользовательского ввода
+def get_token():
+    return str(input('Введите токен аутентификации для Яндекс.Метрика'))
+
+# Получить список необходимых сайтов для выгрузки с пользовательского ввода
+def get_sites():
+    print('Список доступных сайтов для выгрузки (наименование /// id сайта):')
+    for site_id, site_name in sites_dict.items():
+        print(site_name + ' /// ' + site_id)  
+    print()
+    while True:
+        try:
+            results_list = []
+            sites_list = input('Введите через запятую наименования необходимых сайтов для выгрузки').strip().split(',')
+            if len(sites_list) > 0:
+                for input_site_name in sites_list:
+                    for site_id, site_name in sites_dict.items():
+                        if input_site_name.strip() == site_name:
+                            results_list.append(site_id)
+                return results_list
+            else:
+                print('Ввод не может быть пустым значением')
+        except:
+            pass
+
+# Получить даты интервала для выгрузки с пользовательского ввода
+def get_dates():
+    while True:
+        try:            
+            date1 = str(input('Введите первую дату для интервала выгрузки формата YYYY-MM-DD: ')).lower().strip()
+            date2 = str(input('Введите вторую дату для интервала выгрузки формата YYYY-MM-DD: ')).lower().strip()
+            
+            datetime.strptime(date1, '%Y-%m-%d')
+            datetime.strptime(date2, '%Y-%m-%d')
+            if (datetime.strptime(date2, "%Y-%m-%d") - datetime.strptime(date1, "%Y-%m-%d")).days <= 0:
+                print('В интервал должен входить хотя бы один день.')
+            elif (datetime.strptime(date2, "%Y-%m-%d") - datetime.strptime(date1, "%Y-%m-%d")).days >= 365:
+                print('Ширина интервала не должна превышать год.')
+            else:
+                print('Интервал успешно выбран.')
+                return [date1, date2]
+        except:
+            print("Некорректный формат даты, должен быть YYYY-MM-DD.")
+
+# Получить цель выгрузки (Просмотры/Визиты) с пользовательского ввода
+def get_target():
+    while True:
+        try:
+            target = str(input('Введите цель выгрузки (Просмотры/Визиты):')).lower().strip()
+            if target == 'просмотры' or target == 'визиты':
+                return target
+            else:
+                print('Некорректно введена цель, должна быть "Просмотры" или "Визиты"')
+        except:
+            pass
