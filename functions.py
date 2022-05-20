642d3e3cb4ea808101c0d3df2ad1faf13230ffdf
wtrmlln <ama20012001@gmail.com>
2022-05-20 09:53:00 +0400

    access_token fix + wait_report param added to "client" variable

--- functions.py
+++ functions.py
@@ -8,8 +8,7 @@ from tapi_yandex_metrika import YandexMetrikaLogsapi
 from user_input_functions import token, target, date1, date2
 
 def get_ym_data(site_id):
-    print(token)
-    client = YandexMetrikaLogsapi(token=token, default_url_params={'counterId': site_id})
+    client = YandexMetrikaLogsapi(access_token=token, default_url_params={'counterId': site_id}, wait_report=True)
     
     visits_fields_ym = ",".join(list(visits_columns_dict.keys()))
     hits_fields_ym = ",".join(list(hits_columns_dict.keys()))
@@ -28,27 +27,19 @@ def get_ym_data(site_id):
     result = client.create().post(params=params)
     request_id = result["log_request"]["request_id"]
 
-    # Если запрос подготовлен - скачать и обработать в датафрейм
-    # В ином случае - ячейка выполняется до тех пор пока не будет подготовлен запрос
-    while True:
-        info = client.info(requestId=request_id).get()
-        if info['log_request']['status'] == 'processed':
-            filename = str('YaMetrika ' + target + ' ' + sites_dict[site_id] + ' ' + datetime.datetime.now().strftime('%Y-%m-%d') + '.csv')
-            print(filename + ' подготовлен к скачиванию')
-            part = client.download(requestId=request_id, partNumber=0).get()
-            data = part.data
-            data = [x.split('\t') for x in data.split('\n')[:-1]]
-            df_ym = pd.DataFrame(data[1:], columns=data[0])
-            df_ym = df_ym.rename(columns=lambda x: x.strip())
-            
-            if target == 'visits':
-                df_ym = df_ym.rename(columns = visits_columns_dict)
-            elif target == 'hits':
-                df_ym = df_ym.rename(columns = hits_columns_dict)
-            break
-        else:
-            pass
-    #Преобразовывает числовые идентификаторы в текст и скачивает таблицу в формате xlsx    
+    filename = str('YaMetrika ' + target + ' ' + sites_dict[site_id] + ' ' + datetime.now().strftime('%Y-%m-%d') + '.csv')
+    print(filename + ' подготовлен к скачиванию')
+    part = client.download(requestId=request_id, partNumber=0).get()
+    data = part.data
+    data = [x.split('\t') for x in data.split('\n')[:-1]]
+    df_ym = pd.DataFrame(data[1:], columns=data[0])
+    df_ym = df_ym.rename(columns=lambda x: x.strip())
+    
+    if target == 'visits':
+        df_ym = df_ym.rename(columns = visits_columns_dict)
+    elif target == 'hits':
+        df_ym = df_ym.rename(columns = hits_columns_dict)
+#Преобразовывает числовые идентификаторы в текст и скачивает таблицу в формате xlsx    
     
     df_ym['Идентификаторы достигнутых целей'] = df_ym['Идентификаторы достигнутых целей'].apply(partial(get_id_names, site_id=site_id))
     print(filename + ' успешно обработан в DataFrame')
